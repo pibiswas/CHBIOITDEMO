d5e8c976c39007bbe37ad9589147ca67caad5469
Pinaki Biswas <pibiswas@deloitte.com>
2017-05-22 18:20:23 +0000

    first commit

---
+++ BIOIT2017V1.R
@@ -0,0 +1,33 @@
+# Logistic Regression Model
+
+# Importing the dataset
+dataset = read.delim('C:\\users\\pibiswas\\Desktop\\current work\\bio-it\\rstudio\\BioIT2017_data254OV_SvR_v1.tsv')
+
+dataset = dataset[2:52]
+names(dataset) = sub(" ", ".", names(dataset))
+
+# Encoding categorical data, encoding the target feature as a factor
+dataset$Platinum.Status = factor(dataset$Platinum.Status, levels = c('Resistant', 'Sensitive'), labels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Platinum.Status, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[, 2:51] = scale(training_set[, 2:51])
+test_set[, 2:51] = scale(test_set[, 2:51])
+
+# Fitting Logistic Regression to the Training set
+classifier = glm(formula = Platinum.Status ~ ., family = binomial, data = training_set)
+
+# Predicting the Test set results
+prob_pred = predict(classifier, type = 'response', newdata = test_set[, 2:51])
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 1], y_pred > 0.5)
